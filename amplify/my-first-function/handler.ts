e5eabfb435b7f7115db3bffa43b4cd5e82a6dc08
Yashwanth Peddiraju <ypeddiraju@delta.local>
2024-06-04 21:23:23 +0530

    addedLambda

--- amplify/my-first-function/handler.ts
+++ amplify/my-first-function/handler.ts
@@ -1,5 +1,40 @@
 import { Handler } from "aws-cdk-lib/aws-lambda";
 
-export const handler : Handler = async (event: any) => {
-    return "Hello from my first function!";
-  };
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+const ses = new SESClient({ region: "ap-south-1" });
+
+
+export const handler: Handler = async(event: any,context : any,callback: any) => {
+  const body = event.queryStringParameters.body;
+  const subject = event.queryStringParameters.subject;
+  const toMail: string = event.queryStringParameters.toMail;
+  
+  //console.log(body);
+  
+  const command = new SendEmailCommand({
+    Destination: {
+      ToAddresses: [toMail],
+    },
+    Message: {
+      Body: {
+        Text: { Data: body },
+      },
+
+      Subject: { Data: subject },
+    },
+    Source: "yashwanthpeddiraju007@gmail.com",
+  });
+
+  try {
+    let response = await ses.send(command);
+    // process data.
+    console.log(response["$metadata"]["httpStatusCode"])
+    return response;
+  }
+  catch (error) {
+    callback(new Error("Invalid request"));
+  }
+  finally {
+    // finally.
+  }
+};
