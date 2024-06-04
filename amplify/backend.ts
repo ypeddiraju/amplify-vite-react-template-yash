a542b1389718f2870beae8462845c468414decf3
Yashwanth Peddiraju <ypeddiraju@delta.local>
2024-06-04 22:44:06 +0530

    roleLambdaV2

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,23 +1,23 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { myFirstFunction } from './my-first-function/resource';
+import { myFirstFunctionV2 } from './my-first-function/resource';
 import * as iam from "aws-cdk-lib/aws-iam"
-import * as ses from "aws-cdk-lib/aws-ses"
+import * as ses from "aws-cdk-lib/aws-ses" 
 
 
-defineBackend({
+const backend=defineBackend({
   auth,
   data,
-  myFirstFunction,
+  myFirstFunctionV2,
 })
 
-/* const myFirstFunctionLambda = backend.myFirstFunction.resources.lambda;
+const myFirstFunctionLambda = backend.myFirstFunctionV2.resources.lambda;
 
 myFirstFunctionLambda.addToRolePolicy(new iam.PolicyStatement({
   actions:  ["ses:*"],
   resources: ['*'],
   effect: iam.Effect.ALLOW,
 }));
- */
+ 
 
--- amplify/my-first-function/resource.ts
+++ amplify/my-first-function/resource.ts
@@ -1,6 +1,6 @@
 import { defineFunction } from "@aws-amplify/backend";
     
-export const myFirstFunction = defineFunction({
+export const myFirstFunctionV2 = defineFunction({
   name: "my-first-function",
   entry: "./handler.ts"
 });
