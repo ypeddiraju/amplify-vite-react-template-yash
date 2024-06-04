61fe648d6079cb43f2c01c65ec6bf716d2376ad5
Yashwanth Peddiraju <ypeddiraju@delta.local>
2024-06-04 09:14:24 +0530

    withoutLambda

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-const client = generateClient<Schema>();
+import { useState } from "react";
+//import type { Schema } from "../amplify/data/resource";
+//import { generateClient } from "aws-amplify/data";
+
+//const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  /* const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -16,14 +19,127 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+  function deleteTodo(id:string){
+    client.models.Todo.delete({id});
+  } */
+  const [bodyValue, setBodyValue] = useState("");
+  const [toMailValue, setToMailValue] = useState("");
+  const [subjectValue, setSubjectValue] = useState("");
+  //const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  function handleSubmit(event: { preventDefault: () => void; }) {
+    event.preventDefault();
+    //document.getElementById("demo").innerHTML = bodyValue;
+    const format = {
+      queryStringParameters: {
+        body: bodyValue,
+        toMail: toMailValue,
+        subject: subjectValue,
+      },
+    };
+
+    let url =
+      "https://cmisth8zzc.execute-api.ap-south-1.amazonaws.com/test1/everytimeusingnode";
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(format),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+          //setShowSuccessMessage(false);
+          alert("mail sent successfully");
+        
+        return data.json();
+      })
+      .catch(() => null);
+  }
+  const mystyle = {
+    padding: "100px 10px 10px 10px",
+  };
 
   return (
-    <main>
-      <h1>My todos</h1>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>{user?.signInDetails?.loginId}</h1>
+          <div style={mystyle}>
+            <form
+              onSubmit={handleSubmit}
+              style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+            >
+              <label>
+                To Mail:
+                <input
+                  type="mail"
+                  value={toMailValue}
+                  onChange={(event) => setToMailValue(event.target.value)}
+                  style={{
+                    padding: "5px",
+                    marginRight: "10px",
+                    border: "1px solid #ccc",
+                    width: "500px",
+                  }}
+                />
+              </label>
+              <br />
+              <label>
+                Subject:
+                <input
+                  type="text"
+                  value={subjectValue}
+                  onChange={(event) => setSubjectValue(event.target.value)}
+                  style={{
+                    padding: "5px",
+                    marginRight: "10px",
+                    border: "1px solid #ccc",
+                    width: "500px",
+                  }}
+                />
+              </label>
+              <br />
+              <label>Body:</label>
+              <textarea
+                value={bodyValue}
+                onChange={(event) => setBodyValue(event.target.value)}
+                rows={10}
+                cols={80}
+                wrap={"pre"}
+                style={{
+                  padding: "5px",
+                  border: "1px solid #ccc",
+                  resize: "vertical",
+                  whiteSpace: "pre",
+                }}
+              />
+
+              <br />
+
+              <button
+                type="submit"
+                style={{
+                  padding: "10px",
+                  backgroundColor: "#4CAF50",
+                  color: "white",
+                  border: "none",
+                  cursor: "pointer",
+                }}
+              >
+                Send Mail
+              </button>
+              {/* <span>{showSuccessMessage && <h3> hola</h3>}</span>
+              {showSuccessMessage && (
+        <div id="successMessage" style={{ display: 'block' }}>
+          Unverified mail
+        </div>
+      )} */}
+            </form>
+          </div>
+          {/* <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li key={todo.id} onClick={()=>{deleteTodo(todo.id)}} >{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -32,8 +148,11 @@ function App() {
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
         </a>
-      </div>
-    </main>
+      </div> */}
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
