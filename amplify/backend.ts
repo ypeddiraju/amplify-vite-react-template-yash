49ed07e7384543a6e26e31d0ccb3f69ad5da1a1f
Yashwanth Peddiraju <ypeddiraju@delta.local>
2024-06-04 22:34:47 +0530

    noroleLambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -6,19 +6,18 @@ import * as iam from "aws-cdk-lib/aws-iam"
 import * as ses from "aws-cdk-lib/aws-ses"
 
 
-
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
   myFirstFunction,
 })
 
-const myFirstFunctionLambda = backend.myFirstFunction.resources.lambda;
+/* const myFirstFunctionLambda = backend.myFirstFunction.resources.lambda;
 
 myFirstFunctionLambda.addToRolePolicy(new iam.PolicyStatement({
   actions:  ["ses:*"],
   resources: ['*'],
   effect: iam.Effect.ALLOW,
 }));
-
+ */
 
